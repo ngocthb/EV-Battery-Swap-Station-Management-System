ef74adae027b1913c9a6278686bb83abe92374dc
Ngọc Trần <ngoctran250426@gmail.com>
2025-10-12 19:35:42 +0700

    thêm favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "./globals.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Providers from "./Providers";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "amply - Hệ thống quản lý trạm thông minh",
+  icons: {
+    icon: "/logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body>
         <Providers>{children}</Providers>
       </body>
