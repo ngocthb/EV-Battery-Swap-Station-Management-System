071973fd7895d12efd3444731b5f19239fcee880
nnthach <nguyenngocthach2301@gmail.com>
2025-10-25 22:04:51 +0700

    Edit sth

--- src/app/(public)/verify-email/page.tsx
+++ src/app/(public)/verify-email/page.tsx
@@ -1,32 +1,12 @@
 "use client";
-import authService from "@/services/authService";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+
+import { Suspense } from "react";
+import VerifyEmailForm from "./component/VerifyEmailForm";
 
 export default function VerifyEmailPage() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const router = useRouter();
-
-  const handleVerifyEmail = async (token: string) => {
-    try {
-      const res = await authService.verifyEmail(token);
-      console.log("Email verified successfully:", res.data);
-      router.push("/login");
-    } catch (error) {
-      console.log("Error verifying email:", error);
-    }
-  };
-  useEffect(() => {
-    if (token) {
-      console.log("Verifying email with token:", token);
-      handleVerifyEmail(token);
-    }
-  }, [token]);
-
   return (
-    <div>
-      <h1>Đang xác thực...</h1>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <VerifyEmailForm />
+    </Suspense>
   );
 }
